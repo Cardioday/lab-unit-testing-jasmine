d08e2133cb9587b2a2f4bef3c8915c16f2639bad
Michael Giannini <giannini.michaelg@gmail.com>
2024-04-14 23:04:38 +0200

    Solved Lab

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,10 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      it("should returned undefined if any of the arguments is not a number", () => {
+        expect(add("a", "b")).toEqual(undefined);
+        expect(add("a")).toEqual(undefined);
+      })
     });
   });
   
